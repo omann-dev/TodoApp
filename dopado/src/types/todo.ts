465395cb3872330a3e8a63e8c34540b35444996e
omann-dev <oliver.mann007@gmail.com>
2026-06-03 12:32:29 +0200

    Added folder structure for MVP and todo type

---
+++ dopado/src/types/todo.ts
@@ -0,0 +1,11 @@
+
+export type Todo = {
+    id: string;
+    title: string;
+    description: string | null;
+    plannedFor: string;
+    createdAt: string;
+    completedAt: string | null;
+    deletedAt: string | null;
+    idDone: boolean;
+}
